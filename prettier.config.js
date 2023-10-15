e7b7d7c8ea9e17fa392f667a72cbbc0a0542feb0
Diego Ribeiro <diegoribeirodeveloper@gmail.com>
2023-10-15 20:36:32 -0300

    feat: config prettier and lint

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	singleQuote: true,
+	// tralingComma: 'all',
+	arrowParens: 'avoid',
+	endOfLine: 'auto',
+	printWidth: 80,
+	semi: false,
+	tabWidth: 2,
+	trailingComma: 'all',
+	useTabs: true,
+}
